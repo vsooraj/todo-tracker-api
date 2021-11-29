bb925b923665af96e6f0719124f4973bd7ab5d55
vsooraj <vsooraj@gmail.com>
2021-11-29 18:36:22 +0530

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/health", (req, res) => {
+  res.send("I am up and running!");
+});
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(
     `Heroku  to-do application  listening at http://localhost:${port}`
   );
